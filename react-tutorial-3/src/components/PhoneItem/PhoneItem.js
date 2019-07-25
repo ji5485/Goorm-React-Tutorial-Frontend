be5b9f2f4025286e53e702445359c1ab3dafd714
ji5485 <ji5485@naver.com>
2019-07-26 07:00:02 +0900

    Adjust Several Styles

--- react-tutorial-3/src/components/PhoneItem/PhoneItem.js
+++ react-tutorial-3/src/components/PhoneItem/PhoneItem.js
@@ -1,14 +1,14 @@
 import React from "react";
-import "./PhoneItem.css";
+import styles from "./PhoneItem.module.scss";
 
 const PhoneItem = () => {
   return (
-    <div className="phone_item">
-      <div className="phone_item_left">
-        <div className="phone_item_name">홍길동</div>
-        <div className="phone_item_phone">010-0000-0000</div>
+    <div className={styles.phone_item}>
+      <div className={styles.phone_item_left}>
+        <div className={styles.phone_item_name}>홍길동</div>
+        <div className={styles.phone_item_phone}>010-0000-0000</div>
       </div>
-      <div className="phone_item_right">
+      <div className={styles.phone_item_right}>
         <button>삭제</button>
       </div>
     </div>
