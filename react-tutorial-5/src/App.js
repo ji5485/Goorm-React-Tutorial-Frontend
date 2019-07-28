cd7132892746a16fcc231966caff6d9bd65bd33b
ji5485 <dong5485@gmail.com>
2019-07-29 07:40:27 +0900

    Update React Tutorial 5

---
+++ react-tutorial-5/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import InputBox from "./components/InputBox";
+import PhoneList from "./components/PhoneList";
+import "./App.css";
+import { dummyData, nextId, setNextId } from "./lib/dummyData.js";
+
+class App extends Component {
+  state = {
+    dummyData,
+    name: "",
+    phone: ""
+  };
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    const { dummyData, name, phone } = this.state;
+
+    if (name === "" || phone === "") return;
+
+    this.setState({
+      dummyData: {
+        ...dummyData,
+        [nextId]: {
+          id: nextId,
+          name,
+          phone
+        }
+      },
+      name: "",
+      phone: ""
+    });
+
+    setNextId();
+  };
+
+  handleRemove = id => {
+    const { dummyData } = this.state;
+    delete dummyData[id];
+
+    this.setState({ dummyData });
+  };
+
+  render() {
+    const { handleInput, handleSubmit, handleRemove } = this;
+    const { dummyData, name, phone } = this.state;
+
+    return (
+      <div className="container">
+        <InputBox
+          name={name}
+          phone={phone}
+          onChange={handleInput}
+          onSubmit={handleSubmit}
+        />
+        <PhoneList list={dummyData} deleteItem={handleRemove} />
+      </div>
+    );
+  }
+}
+
+export default App;
