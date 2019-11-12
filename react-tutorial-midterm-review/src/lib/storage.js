fdd535bf4a5e21007b71ff431698e332c1bd9599
Ju Hyeon Do <ji5485@naver.com>
2019-11-12 19:53:27 +0900

    Change Gitignore

---
+++ react-tutorial-midterm-review/src/lib/storage.js
@@ -0,0 +1,32 @@
+// https://github.com/vlpt-playground/heurm/blob/master/heurm-client/src/lib/storage.js
+
+const storage = {
+  set: (key, object) => {
+    if (!localStorage) return;
+    localStorage[key] =
+      typeof object === "string" ? object : JSON.stringify(object);
+  },
+  get: key => {
+    if (!localStorage) return null;
+
+    if (!localStorage[key]) {
+      return null;
+    }
+
+    try {
+      const parsed = JSON.parse(localStorage[key]);
+      return parsed;
+    } catch (e) {
+      return localStorage[key];
+    }
+  },
+  remove: key => {
+    if (!localStorage) return null;
+
+    if (localStorage[key]) {
+      localStorage.removeItem(key);
+    }
+  }
+};
+
+export default storage;
